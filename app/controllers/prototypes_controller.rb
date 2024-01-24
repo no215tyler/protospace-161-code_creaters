ad31de38bddab7b131f4f8d0b6e0c1cec52955a5
mackey3690 <sports.is.bowling@gmail.com>
2024-01-24 17:20:30 +0900

    投稿機能実装1

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -17,7 +17,7 @@ class PrototypesController < ApplicationController
   private
 
   def prototype_params
-  params.require(:prototype).permit(:title, :image).merge(user_id: current_user.id)
+  params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
 end
 
 end
