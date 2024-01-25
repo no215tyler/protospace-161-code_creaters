e079fa0616d9d3938a7ed2fb61a57ef634bf6180
no215tyler <no.215.tyler@gmail.com>
2024-01-25 23:26:05 +0900

    Merge pull request #15 from no215tyler/一覧表示機能の実装
    一覧表示機能の実装の途中まで

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,5 @@
 class Prototype < ApplicationRecord
+  belongs_to :user
   has_one_attached :image
   
   validates :title, :catch_copy, :concept,  presence: true
@@ -6,5 +7,4 @@ class Prototype < ApplicationRecord
   def was_attached?
     self.image.attached?
   end
-
 end
