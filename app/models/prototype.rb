9c9be7776063854ea0bb77d2c5c4fcbb1297d61b
no215tyler <no.215.tyler@gmail.com>
2024-01-26 16:38:11 +0900

    画像添付を必須とするバリデーションを追加

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,9 +2,5 @@ class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   
-  validates :title, :catch_copy, :concept,  presence: true
-  
-  def was_attached?
-    self.image.attached?
-  end
+  validates :title, :catch_copy, :concept, :image, presence: true
 end
