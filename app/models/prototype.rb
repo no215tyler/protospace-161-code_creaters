a024e456a7d0325f1bd08861f70bab8925000ee9
no215tyler <no.215.tyler@gmail.com>
2024-01-28 16:37:10 +0900

    スペルミス修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,7 @@
 class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  has_many :commnets, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :title, :catch_copy, :concept, :image, presence: true
 end
